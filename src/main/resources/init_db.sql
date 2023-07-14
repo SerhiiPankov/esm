7cd914e05af52bdff4646f690e422be164f55a49
PankovSerhii <pankoff.serg@gmail.com>
2023-07-14 18:03:15 +0300

    added scripts for create tables (GiftCertificate&Tag_GiftCertificate)

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -11,3 +11,25 @@ CREATE TABLE `Tag` (
                        UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB CHARSET=utf8;
 
+DROP TABLE IF EXISTS `GiftCertificate`;
+CREATE TABLE `GiftCertificate` (
+                       `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                       `name` varchar(64) NOT NULL,
+                       `description` varchar(255) NOT NULL,
+                       `price` decimal(10,2) NOT NULL,
+                       `duration` int(11) NOT NULL,
+                       `createDate` datetime NOT NULL,
+                       `lastUpdateDate` datetime NOT NULL,
+                       PRIMARY KEY (`id`),
+                       UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB CHARSET=utf8;
+
+
+CREATE TABLE `Tag_GiftCertificate` (
+                                       `gift_certificate_id` bigint(20) NOT NULL,
+                                       `tag_id` bigint(20) NOT NULL,
+                                       UNIQUE KEY `gift_certificate_id` (`gift_certificate_id`,`tag_id`),
+                                       KEY `fk_tag_id` (`tag_id`),
+                                       CONSTRAINT `fk_gift_certificate_id` FOREIGN KEY (`gift_certificate_id`) REFERENCES `GiftCertificate` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+                                       CONSTRAINT `fk_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `Tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
